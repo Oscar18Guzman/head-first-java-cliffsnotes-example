06af24e2eed00b0dba21e69f06429ad34d814e5c
Oscar Guzman <oguzman@basecampcodingacademy.org>
2020-01-29 09:37:14 -0600

    Finalyzed Chapter 2

---
+++ chapter-2-A-trip-to-objectville/ParentOverriding.java
@@ -0,0 +1,30 @@
+class ParentOverriding {
+    void m1() {
+        System.out.println("From parent " + "static m1()");
+    }
+
+    void m2() {
+        System.out.println("From parent " + "non-static(instance) m2()");
+    }
+}
+
+class Child extends Parent {
+    static void m1() {
+        System.out.println("From child static m1()");
+    }
+
+    @Override
+    public void m2() {
+        System.out.println("From child " + "non-static(instance) m2()");
+    }
+}
+
+// Driver class
+class Main {
+    public static void main(String[] args) {
+        ParentOverriding obj1 = new Child();
+
+        obj1.m1();
+        obj1.m2();
+    }
+}
